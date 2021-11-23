315fe93f69e5c8b5c1a6422a71f389611494e785
jonasstrube <26897240+jonasstrube@users.noreply.github.com>
2021-11-23 16:12:05 +0100

    fix component

--- public/lib/app.js
+++ public/lib/app.js
@@ -3,4 +3,12 @@ const domTarget = document.getElementById('react-app');
 const myJSXElement = /*#__PURE__*/React.createElement("p", {
   id: "my-id"
 }, "Das ist meine erstes JSX Element");
+
+class App extends React.Component {
+  render() {
+    return /*#__PURE__*/React.createElement("p", null, "Das ist ein React Component!");
+  }
+
+}
+
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), domTarget);
--- src/app.jsx
+++ src/app.jsx
@@ -7,4 +7,10 @@ const myElement = React.createElement(
 const domTarget = document.getElementById('react-app')
 const myJSXElement = <p id='my-id'>Das ist meine erstes JSX Element</p>
 
+class App extends React.Component {
+    render() {
+        return <p>Das ist ein React Component!</p>
+    }
+}
+
 ReactDOM.render(<App/>, domTarget);
